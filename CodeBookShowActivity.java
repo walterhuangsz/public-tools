0f033982fce8385fc3faad80c669090ada12668b
WalterHuang <walter1314007@163.com>
2013-09-11 10:34:13 +0800

    add git attribute file

---
+++ CodeBookShowActivity.java
@@ -0,0 +1,50 @@
+package com.antiysz.activity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import com.antiysz.adapter.CodeBookShowAdapter;
+import com.antiysz.app.MagicRingApplication;
+import com.antiysz.base.BaseActivity;
+import com.antiysz.bean.CodeBookItem;
+import com.antiysz.db.CodeBookDB;
+import com.antiysz.magicring.R;
+
+public class CodeBookShowActivity extends BaseActivity {
+
+	private static final String TAG = "CodeBookShowActivity";
+
+	private ListView showLV;
+	private List<CodeBookItem> list = new ArrayList<CodeBookItem>();
+	public static CodeBookShowActivity instance;
+
+	/**
+	 * init
+	 */
+	private void setUI() {
+
+		instance = this;
+
+		showLV = (ListView) findViewById(R.id.code_book_show_lv);
+		list = new CodeBookDB(this).queryAll();
+		showLV.setAdapter(new CodeBookShowAdapter(this, list));
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.code_book_show);
+		MagicRingApplication.getInstance().addActivity(this);
+		setUI();
+	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		MagicRingApplication.getInstance().removeActivity(this);
+	}
+}
